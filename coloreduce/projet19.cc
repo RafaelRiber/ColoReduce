1a17df1d01fa719360e403c795e738ef6a8dc8d1
RafaelRiber <rafael@riber.ch>
2018-11-05 21:17:48 +0100

    Implement error handling
    Also added debug print for reduced color table, and changed threshold table from float to double as per specs.

--- coloreduce/projet19.cc
+++ coloreduce/projet19.cc
@@ -4,9 +4,11 @@
 #include <iostream>
 #include <vector>
 #include <array>
+#include <cmath>
 using namespace std;
 
 const int MAX = 255;
+const double EPSILON = 0.001;
 
 typedef vector<vector<array<int, 3>>> RGBImg;
 
@@ -14,7 +16,7 @@ typedef vector<vector<array<int, 3>>> RGBImg;
 struct InputImage{
 	int nBr;
 	vector<array<int, 3>> reducedColors = {{0,0,0}};
-	vector<float> thresholds = {0.0, 0.0};
+	vector<double> thresholds = {0.0, 0.0};
 	int nBfilters;
 	string header;
 	int nbC;
@@ -32,52 +34,77 @@ InputImage fileRead();
 
 int main()
 {
-
   InputImage IMG = fileRead(); //TODO-> ERROR HANDLING
 	return 0;
 }
 
-void error_nbR(int nbR)
+void error_nbR(int nbR) //OK
 {
 	cout << "Invalid number of colors: " << nbR << endl;
 }
 
-void error_color(int id)
+void error_color(int id) //OK
 {
 	cout << "Invalid color value " << id << endl;
 }
 
-//DELTA!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-void error_threshold(double invalid_val)
+void error_threshold(double invalid_val) //OK
 {
 	cout << "Invalid threshold value: " << invalid_val << endl;
 }
 
-void error_nb_filter(int nb_filter)
+void error_nb_filter(int nb_filter) //OK
 {
 	cout << "Invalid number of filter: " << nb_filter << endl;
+}
+
+void error_filetype(string header)
+{
+	cout << "Invalid filetype header: " << header << endl;
 }
 
 InputImage fileRead(){
   InputImage IN;
 
-	//Nombre reduit de couleurs
+	//Nombre reduit de couleurs - min 2, max 255
 	cin >> IN.nBr;
+	if(IN.nBr < 2 or IN.nBr > MAX) error_nbR(IN.nBr);
 
 	//valeur des couleurs réduites
-	for (int i = 0; i <= IN.nBr - 1; i++){
+	for (int i = 1; i <= IN.nBr; i++){
 		for (int j = 0; j <= 2; j++){
 			cin >> IN.reducedColors[i][j];
+			if(IN.reducedColors[i][j] < 0 or IN.reducedColors[i][j] > MAX){
+				 error_color(IN.reducedColors[i][j]);
+			 }
 		}
 	}
+
+/*
+	//DEBUG: PRINT TABLE
+	cout << "REDUCED COLOR TABLE:" << endl;
+	for (int i = 0; i <= IN.nBr; i++){
+		for (int j = 0; j <= 2; j++){
+			cout << IN.reducedColors[i][j] << endl;
+		}
+		cout << endl;
+	}
+*/
 
 	//Entrée des nBr - 1 seuils
 	for (int i = 0; i < IN.nBr - 1; i++){
 		cin >> IN.thresholds[i];
+		if (i >= 1){
+			double deltaThresholds = abs(IN.thresholds[i] - IN.thresholds[i-1]);
+			if(deltaThresholds < EPSILON) error_threshold(IN.thresholds[i]);
+		}
 	}
 
 	cin >> IN.nBfilters;
+	if (IN.nBfilters <= 0) error_nb_filter(IN.nBfilters);
+
 	cin >> IN.header;
+	if (IN.header != "P3") error_filetype(IN.header);
 	cin >> IN.nbC;
 	cin >> IN.nbL;
 	cin >> IN.MAX;
