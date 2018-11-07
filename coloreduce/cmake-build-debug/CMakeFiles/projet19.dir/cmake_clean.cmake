ffabfcd5673a590b552837d876e896954f56a492
RafaelRiber <rafael@riber.ch>
2018-11-07 23:39:19 +0100

    CLion Migration

---
+++ coloreduce/cmake-build-debug/CMakeFiles/projet19.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projet19.dir/projet19.cc.o"
+  "projet19.pdb"
+  "projet19"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projet19.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
